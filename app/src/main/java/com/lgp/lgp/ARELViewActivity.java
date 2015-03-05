5c0f844940c33bfeb85bc9e91af6868853f90b5c
carlosmtx <cmcmteixeira@gmail.com>
2015-03-05 15:37:00 +0000

    First Commit

---
+++ app/src/main/java/com/lgp/lgp/ARELViewActivity.java
@@ -0,0 +1,24 @@
+// Copyright 2007-2013 Metaio GmbH. All rights reserved.
+package com.lgp.lgp;
+
+import android.view.View;
+
+import com.metaio.sdk.ARELActivity;
+
+public class ARELViewActivity extends ARELActivity
+{
+
+    @Override
+    protected int getGUILayout()
+    {
+
+        // Attaching layout to the activity
+        return R.layout.template;
+    }
+
+    public void onButtonClick(View v)
+    {
+        finish();
+    }
+
+}
